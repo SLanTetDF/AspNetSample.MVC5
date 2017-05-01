484f125236bb536417bbb79f06c114e98366d364
SLanTetDF <380507482@qq.com>
2017-05-01 22:44:18 +0800

    添加项目文件。

---
+++ AspNetSample.MVC5/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetSample.MVC5
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
